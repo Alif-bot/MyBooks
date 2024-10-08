2595f88d78391756b54fc8feab324023945a7a44
Md Alif Hossain <alif.cse344@gmail.com>
2024-10-08 18:39:41 +0600

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Md Alif Hossain on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
